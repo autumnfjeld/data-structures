93e59e45698335d0c6ba21fea5123538d40e1ec2
Autumn <nmutua@gmail.com>
2014-01-08 14:56:55 -0800

    Added variable to keep track of size and modified push function to increment size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,21 +1,24 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
+  var storage = {};
+  var size = 0;
   /* START PROMPT
     var size; // Hint: set an initial value here
   -END PROMPT */
 
   // Implement the methods below
   instance.push = function(value){
+    size++;  
   };
 
   instance.pop = function(){
   };
 
   instance.size = function(){
+    return size;
   };
-  
-    return instance;
-  };
+
+  return instance;
+};
