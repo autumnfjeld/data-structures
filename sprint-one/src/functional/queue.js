63a6bb465476b473260f9fdbfe1beef4f395d86f
Autumn <nmutua@gmail.com>
2014-01-08 15:37:14 -0800

    Finished queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,17 +1,28 @@
 var makeQueue = function(){
   var instance = {};
   var size = 0;
+  var newest = 0;
+  var oldest = 1;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    newest++;
     size++;
+    storage[newest] = value;
   };
 
   instance.dequeue = function(){
-    size && size--;
+    if(size){
+      //want to remove and return oldest
+      temp = storage[oldest];
+      delete storage[oldest];
+      oldest++;
+      size--;
+      return temp;
+    }
   };
 
   instance.size = function(){
@@ -20,3 +31,4 @@ var makeQueue = function(){
 
   return instance;
 };
+
