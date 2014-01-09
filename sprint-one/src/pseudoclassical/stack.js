957d6f7c158f8bb1c07f5d1832d00b27d2f8a7f4
Autumn <nmutua@gmail.com>
2014-01-08 21:25:38 -0800

    Implemented pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,27 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  //this.instance = {};
+  this._storage = {};
+  this._size = 0;
+
 };
+
+Stack.prototype.push = function(value){
+  this._size++;
+  this._storage[this._size] = value;
+};
+
+Stack.prototype.pop = function(){
+  if (this._size){
+    var temp = this._storage[this._size];
+    delete this._storage[this._size];
+    this._size--;
+    return temp;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this._size;
+};
+
+var instance = new Stack();
+
