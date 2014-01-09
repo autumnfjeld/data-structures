bbd00733764234e3b85317de5e334077f2d8d40a
Autumn <nmutua@gmail.com>
2014-01-09 11:20:50 -0800

    Conquered some tests but might have to redo

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,22 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+
+    var incoming = makeNode(value);
+
+    if (list.head === null) {
+      list.head = incoming;
+      list.tail = incoming;
+    }
+    else {
+    // need the new item to alter the previous item
+      list.tail.next = incoming.value;
+      list.tail = incoming;
+    }
   };
 
   list.removeHead = function(){
+    var nextToHead =  
   };
 
   list.contains = function(target, node){
