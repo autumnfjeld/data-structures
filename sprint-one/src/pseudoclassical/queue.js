74c06e04218bed30c2296ab572df004fcd08a8a6
Autumn <nmutua@gmail.com>
2014-01-09 10:08:08 -0800

    Implemeted pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,35 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
-};
+
+  this._size = 0;
+  this._newest = 0;
+  this._oldest = 1;
+  this._storage = {};
+
+  };
+
+    // Implement the methods below
+
+  Queue.prototype.enqueue = function(value){
+    this._newest++;
+    this._size++;
+    this._storage[this._newest] = value;
+  };
+
+  Queue.prototype.dequeue = function(){
+    if(this._size){
+      //want to remove and return oldest
+      temp = this._storage[this._oldest];
+      delete this._storage[this._oldest];
+      this._oldest++;
+      this._size--;
+      return temp;
+    }
+  };
+
+  Queue.prototype.size = function(){
+    return this._size;
+  };
+
+
+
+  var instance = new Queue();
