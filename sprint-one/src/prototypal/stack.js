c1d2f010b80cbda7f7a25098daaaf500c81fcd11
Autumn <nmutua@gmail.com>
2014-01-08 20:46:05 -0800

    implemeted prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,31 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance._storage = {};
+  instance._size = 0;
+
+  return instance;
 };
 
 var stackMethods = {};
+
+
+stackMethods.push = function(value){
+  this._size++;
+  this._storage[this._size] = value;
+};
+
+stackMethods.pop = function(){
+  if (this._size){
+    var temp = this._storage[this._size];
+    delete this._storage[this._size];
+    this._size--;
+    return temp;
+  }
+};
+
+stackMethods.size = function(){
+  return this._size;
+};
