b7cd0aafe85344f38ee74edd3b57ae0364b4a30f
Autumn <nmutua@gmail.com>
2014-01-09 18:10:03 -0800

    should have methods named 'addChild' and 'contains', and a property named 'value'

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,15 +2,22 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  extend(newTree, treeMethods);
   return newTree;
 };
 
-
-var treeMethods = {};
+var treeMethods = {
+};
 
 treeMethods.addChild = function(value){
 };
 
 treeMethods.contains = function(target){
+};
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
 };
 
